ed1dc3555326c2062a251acd504184819e42daed
error0001 <error00010001@gmail.com>
2018-07-16 10:53:03 +0300

    origin

--- IMotor.h
+++ IMotor.h
@@ -1,7 +1,7 @@
 #pragma once
 /*
  * IMotor.h - All motor interface for Wiring/STMElectronics - Version 0.1
- * Thanks next autors Tom Igoe, Sebastian Gassner, David Mellis, Noah Shibley, Eugene Kozlenko, Ryan Orendorff
+ * Thanks next autors: Tom Igoe, Sebastian Gassner, David Mellis, Noah Shibley, Eugene Kozlenko, Ryan Orendorff
  * Original libriary Stepper.h using as example Arduino (ver. 1.1.0)
  * 
  * IMotor for STM32
@@ -14,24 +14,10 @@
 */
 class IMotor
 {
+public:
     IMotor() {} // Empty constructor
-    virtual void Init() = 0; // Init all some variables or modules
     virtual void SetSpeed() = 0;
     virtual void Step() = 0;
-    virtual ~IMotor() {}
-}
-
-class StepMotor : public IMotor
-{
-public:
-    StepMotor(); // Empty constructor
-    StepMotor(uint8_t inNumOfSteps, uint8_t motorPin1,  uint8_t motorPin2); // 2 pins connection
-    StepMotor(uint8_t inNumOfSteps, uint8_t motorPin1,  uint8_t motorPin2,
-                                    uint8_t motorPin3,  uint8_t motorPin4); // 4 pins connection
-    StepMotor(uint8_t inNumOfSteps, uint8_t motorPin1,  uint8_t motorPin2, 
-                                    uint8_t motorPin3,  uint8_t motorPin4,
-                                    uint8_t motorPin5); // 5 pins connection
-    ~StepMotor();
-private:
-    uint8_t numberOfSteps
+    // Add a virtual reconstructor !!!
+    ~IMotor() {}
 }

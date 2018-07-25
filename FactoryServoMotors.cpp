f870f9e8f85492c043f7e9be984475dc5c318701
error0001 <error00010001@gmail.com>
2018-07-25 23:39:36 +0300

    origin

--- FactoryServoMotors.cpp
+++ FactoryServoMotors.cpp
@@ -4,7 +4,15 @@ FactoryServoMotors::FactoryServoMotors()
 {
 }
 
+FactoryServoMotors::FactoryServoMotors(const FactoryServoMotors& copyFactory)
+{
+}
+
 IMotor* FactoryServoMotors::CreateMotor()
 {
 	return new ServoMotor;
+}
+
+FactoryServoMotors::~FactoryServoMotors()
+{
 }
--- FactoryServoMotors.h
+++ FactoryServoMotors.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "FactoryMotors.h"
-
+#include "ServoMotor.h"
 class FactoryServoMotors : public FactoryMotors
 {
 public:
-	FactoryServoMotors();
-	IMotor * CreateMotor();
-private:
-
+	FactoryServoMotors();		//	Clear constructor
+	FactoryServoMotors(const FactoryServoMotors&);	//	Constructor of copy 
+	//FactoryServoMotors& operator=(const FactoryServoMotors&);  //  Assigment operator
+	IMotor* CreateMotor();  //	Create exemplar ServoMotor
+	~FactoryServoMotors();	//	Destructor
 };

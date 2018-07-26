6f43cb0803785567235f87d0db726840264f5ce3
error0001 <error00010001@gmail.com>
2018-07-26 14:16:55 +0300

    origin

--- ServoMotor.h
+++ ServoMotor.h
@@ -4,36 +4,22 @@
  * Date:    2018 16 07
  * 
 */
-
 #pragma once
 #include "IMotor.h"
-#include <stdint.h>
 class ServoMotor : public IMotor
 {
-public:
-	ServoMotor();
-	void Info();
-	/*
-    ServoMotor();
-    ServoMotor(const uint8_t&, const uint16_t&, const uint16_t&);
-    //  Start motor in the mode of test 
-    void Initialize();
-    //  This method is for turning the servo mechanism to a certain angle
-    void SetAngle(const uint16_t&);
-    //  This method is for turning the servo mechanism to a certain pulse width Microsends
-    void SetMicroseconds(uint16_t); 
-    //  This method is for get angle
-    uint16_t GetAngle();
-    //  This method is for get pulse width microseconds
-    uint16_t GetMicroseconds();
-		uint16_t ReadAngle();
-    ~ServoMotor();
 private:
-    uint8_t pinChanel;          //  PWM pin
-    uint16_t minMicroseconds;   //  Minimal value of pulse width microseconds
-    uint16_t maxMicroseconds;   //  Maximal value of pulse width microseconds
-		uint16_t valueAngle;				//	Setting value angle
-		uint16_t valueMicroseconds;	//	
-		uint16_t actualAngle;				//	Angle at driver
-		*/
+	uint16_t pinPwm;
+public:
+	ServoMotor() : IMotor() // Clear constructors
+	{
+	}
+	void Info()
+	{
+		std::cout << "Servo motor is created!" << std::endl;
+	}
+	void AttachPinPwm(uint16_t &inPinPwm, uint16_t &inMinMsec, uint16_t &inMaxMsec)
+	{
+	
+	}
 };

c453ff222e3f123977020c698f2d02aa773d5e66
error0001 <error00010001@gmail.com>
2018-07-26 14:14:25 +0300

    origin

--- FactoryMotors.h
+++ FactoryMotors.h
@@ -1,11 +1,8 @@
 #pragma once
 #include "IMotor.h"
-#include "ServoMotor.h"
-#include "StepMotor.h"
-
 class FactoryMotors
 {
     public:
         virtual IMotor* CreateMotor() = 0;
-				virtual ~FactoryMotors();
+				virtual ~FactoryMotors() {}
 };

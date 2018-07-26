ff3ea66cda4de5acba346f06f5bc0f9cba1577fb
error0001 <error00010001@gmail.com>
2018-07-26 14:18:49 +0300

    origin

--- FactoryServoMotors.h
+++ FactoryServoMotors.h
@@ -6,7 +6,7 @@ class FactoryServoMotors : public FactoryMotors
 public:
 	FactoryServoMotors();		//	Clear constructor
 	FactoryServoMotors(const FactoryServoMotors&);	//	Constructor of copy 
-	//FactoryServoMotors& operator=(const FactoryServoMotors&);  //  Assigment operator
+	//	FactoryServoMotors& operator=(const FactoryServoMotors&);  //  Assigment operator
 	IMotor* CreateMotor();  //	Create exemplar ServoMotor
 	~FactoryServoMotors();	//	Destructor
 };
